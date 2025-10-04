2e19ba7ee3e7d2253fa97e4e5eeb208e77936234
Kaan Koç <kock@mef.edu.tr>
2025-10-04 17:09:34 +0300

    fix: hono app checkout route

--- examples/hono-app/src/routes/index.ts
+++ examples/hono-app/src/routes/index.ts
@@ -2,6 +2,7 @@ import healthRoutes from './health';
 import productsRoutes from './products';
 import plansRoutes from './plans';
 import subscriptionsRoutes from './subscriptions';
+import checkoutRoutes from './checkout';
 import { Hono } from 'hono';
 
 const baseRoutes = new Hono();
@@ -10,5 +11,6 @@ baseRoutes.route('/health', healthRoutes);
 baseRoutes.route('/products', productsRoutes);
 baseRoutes.route('/plans', plansRoutes);
 baseRoutes.route('/subscriptions', subscriptionsRoutes);
+baseRoutes.route('/checkout', checkoutRoutes);
 
 export default baseRoutes;
